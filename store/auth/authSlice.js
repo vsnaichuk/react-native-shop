8a56336d59dfbb4c9a09ba6da72c90b72e473e3f
v-sn <v.snaichuk@gmail.com>
2021-04-06 14:45:44 +0300

    Implement authSlice with signup and login thunks
    Implement authHandler and API Auth endpoints. Minor fix

---
+++ store/auth/authSlice.js
@@ -0,0 +1,94 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Api } from '../../api/Api';
+
+export const createOrder = createAsyncThunk(
+  'orders/createOrder',
+  async ({ orderItems, totalAmount }, { rejectWithValue }) => {
+    try {
+      const res = await Api.createOrder({
+        orderItems,
+        totalAmount,
+      });
+
+      if (res.status === 201) {
+        return { ...res.data };
+      } else {
+        return rejectWithValue(res.data);
+      }
+    } catch (e) {
+      console.log('Error', e.response.data);
+      return rejectWithValue(e.response.data);
+    }
+  },
+);
+
+export const fetchOrders = createAsyncThunk(
+  'orders/fetchOrders',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await Api.fetchOrders();
+
+      if (res.status === 201) {
+        return { ...res.data };
+      } else {
+        return rejectWithValue(res.data);
+      }
+    } catch (e) {
+      console.log('Error', e.response.data);
+      return rejectWithValue(e.response.data);
+    }
+  },
+);
+
+export const ordersSlice = createSlice({
+  name: 'orders',
+  initialState: {
+    orders: [],
+    isFetching: false,
+    isSuccess: false,
+    isError: false,
+    errMessage: '',
+  },
+  reducers: {
+    clearState: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isFetching = false;
+      state.errMessage = '';
+
+      return state;
+    },
+  },
+  extraReducers: {
+    [createOrder.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+
+      state.orders.concat(payload.order);
+    },
+    [createOrder.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [createOrder.rejected]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isError = true;
+      state.errMessage = payload;
+    },
+    [fetchOrders.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+
+      state.orders = payload.orders;
+    },
+    [fetchOrders.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [fetchOrders.rejected]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isError = true;
+      state.errMessage = payload;
+    },
+  },
+});
+
+export const { clearState } = ordersSlice.actions;
+
+export const ordersSelector = (state) => state.orders;
